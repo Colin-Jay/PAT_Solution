51c15cbb34300cbc1c67343aec44f72b58bbfe14
colin-jay <1606834740@qq.com>
2020-05-16 21:49:21 +0800

    B1

---
+++ BasicLevel/1001. 害死人不偿命的(3n+1)猜想.cpp
@@ -0,0 +1,16 @@
+#include<cstdio>
+
+int main() {
+    int n, step = 0;
+    scanf("%d", &n);
+    while(!n) {
+        if(n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = (3 * n + 1) / 2;            
+        }
+        step++;
+    }
+    printf("%d", step);
+    return 0;
+}
