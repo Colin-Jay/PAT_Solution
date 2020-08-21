114d2efc7a56389b29661aff883e678456fc4d28
colin-jay <1606834740@qq.com>
2020-08-22 00:26:56 +0800

    B1020

--- BasicLevel/1020. 月饼.cpp
+++ BasicLevel/1020. 月饼.cpp
@@ -11,18 +11,18 @@ struct mooncake
 } cake[1010];
 
 bool cmp(mooncake a, mooncake b)
-{
+{//按照单价将月饼排序
     return a.price > b.price;
 }
 
 int main()
 {
-    int n; //种类
-    double d; //需求量
+    int n;              //种类
+    double d;           //需求量
     double maxPfit = 0; //利润
     scanf("%d %lf", &n, &d);
     for (int i = 0; i < n; i++)
-    {
+    { 
         scanf("%lf", &cake[i].sum);
     }
     for (int i = 0; i < n; i++)
@@ -33,7 +33,7 @@ int main()
     sort(cake, cake + n, cmp);
     double temp = d; //剩余需求
     for (int i = 0; i < n; i++)
-    {
+    {//第二个条件i<n不能是temp>0，当d足够大可能段错误
         if (temp >= cake[i].sum)
         {
             maxPfit += cake[i].sumPrice;
